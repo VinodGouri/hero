7e29cbb4e7923fb8f37fad106786080e7a71c15e
VinodGouri <vinodgouri0109@gmail.com>
2024-03-18 15:22:04 +0530

    script fileadded and login.html

---
+++ login.js
@@ -0,0 +1,11 @@
+let hidepass = document.getElementById("hide-pass");
+let password = document.getElementById("password");
+hidepass.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        hidepass.src="https://cdn-icons-png.freepik.com/256/14035/14035324.png?ga=GA1.1.617146296.1709204938&";
+    }else{
+        password.type = "password";
+        hidepass.src="https://cdn-icons-png.freepik.com/256/14035/14035312.png?ga=GA1.1.617146296.1709204938&";
+    }
+}
